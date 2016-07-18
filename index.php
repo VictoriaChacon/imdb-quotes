b8773a7dd4f3a2dd8e565b6c70e5bf72de2ecf1c
Victoria Chacon <Vchaco06@gmail.com>
2016-07-18 08:21:20 -0600

    added paragraphs

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vchaco06
- * Date: 7/15/16
- * Time: 10:33 AM
- */
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Data Design</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<p><strong>Name</strong>:Marisa Lucero</p>
+		<br>
+		<p><strong>Age</strong>:24</p>
+		<br>
+		<p><strong>Profession</strong>:Victoria is a student and a part-time worker. She spends a lot of her time dealing with people who know a lot about movies. As a student she is always catching up with friends about the latest movies. </p>
+	</body>
+</html>
