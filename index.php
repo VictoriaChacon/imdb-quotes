320109fdb21be8223a04ae3b304945cefed82865
Victoria Chacon <Vchaco06@gmail.com>
2016-07-20 14:35:38 -0600

    added persona and use case

--- index.php
+++ index.php
@@ -5,21 +5,24 @@
 	</head>
 	<body>
 		<h1>Persona</h1>
-		<p><strong>Name</strong>:Marisa Lucero</p>
+		<p><strong>Name</strong>:Victoria Point</p>
 		<br>
 		<p><strong>Age</strong>:24</p>
 		<br>
-		<p><strong>Profession</strong>:Marisa is a student and a part-time worker. She spends a lot of her time dealing with people who know a lot about movies. As a student she is always catching up with friends about the latest movies. </p>
+		<p><strong>Profession</strong>:Victoria is a student and a part-time worker. She spends a lot of her time dealing with people who know a lot about movies. As a student she is always catching up with friends about the latest movies. </p>
 		<br>
-		<p><strong>Technology</strong>:Marisa is a Mac user but also uses her phone, which is an android device. She spends most of her time on her phone only using her computer when her phone is dead or misplaced. She has limited data connection with her phone because her service only allows high speeds for a period of time.</p>
+		<p><strong>Technology</strong>:Victoria is a Mac user but also uses her phone, which is an android device. She spends most of her time on her phone only using her computer when her phone is dead or misplaced. She has limited data connection with her phone because her service only allows high speeds for a period of time.</p>
 		<br>
-		<p><strong>Attitudes and Behaviors</strong>:Marisa is very busy, she does to scool full time and works in-between her school hours. When she wants information, she wants it fast and to the point. She will spend no more than three minutes looking through a page before she moves on to another source. She is a big fan of social media and likes to share her findings with friends who share her interest.</p>
+		<p><strong>Attitudes and Behaviors</strong>:Victoria is very busy, she does to shcool full time and works in-between her school hours. When she wants information, she wants it fast and to the point. She will spend no more than three minutes looking through a page before she moves on to another source. She is a big fan of social media and likes to share her findings with friends who share her interest.</p>
 		<br>
-		<p><strong>Frustrations and Needs</strong>:Marisa has a big interest in films, however, with her limited amount of time she needs to get information about a film without having to watch it in its entirety.</p>
+		<p><strong>Frustrations and Needs</strong>:Victoria has a big interest in films, however, with her limited amount of time she needs to get information about a film without having to watch it in its entirety.</p>
 		<br>
 		<p><strong>Goals</strong>:It 11:00pm on a Friday night, Victoria is at work. Victoria finally has an hour to do some research before she continues working. She would like to find a couple of facts about the movie she is researching and also have some interesting points to bring up with her coworker. With only an hour Victoria would like to find at least two points to write about within 15 minutes so she can spend the majority of her time writing.
 		</p>
-		<p>this is to  push for now</p>
+		<br>
+		<p><strong>Use Case</strong> :Victoria has decided that she ants to work with a particular movie for her film project. She looks through her list of resources and wants quick information that does not require too many clicks. Right now she just needs tidbits of character information to expand upon.
+		Knowing which film she wants to work on Victoria goes to IMDB a site that she frequents for movie information.
+		Its 11:00pm on  a Friday night, Victoria is at work, but finally has an hour break in order to do some researching. She would like to find a couple of character quotes and expand on them and their actors as well as get some writing done. She only wants to spend maybe fifteen minutes researching and the rest of her time writing. </p>
 	</body>
 </html>
 
