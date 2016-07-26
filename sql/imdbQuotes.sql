c0cfb4c7ec8b1caf39eabcd7a9ca2656cb2f2907
Victoria Chacon <Vchaco06@gmail.com>
2016-07-26 09:59:15 -0600

    Added in foreign keys and up to this point SQL is done.

--- sql/imdbQuotes.sql
+++ sql/imdbQuotes.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS actor;
 DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS quote;
+DROP TABLE IF EXISTS quoteActor;
+DROP TABLE IF EXISTS actorMovie;
 
 -- Creating tables for my entities here
 -- Create actor entity; actorId is the primary key
@@ -12,9 +14,19 @@ CREATE TABLE actor (
 );
 -- Creating weak entity actorMovie here
 -- Many quotes can have many actors
+CREATE TABLE actorMovie (
+	actorMovieActorId INT UNSIGNED NOT NULL,
+	actorMovieMovieId INT UNSIGNED NOT NULL,
+	-- Indexing foreign keys
+	INDEX(actorMovieActorId),
+	INDEX(actorMovieMovieId),
+	-- Foreign key relations
+	FOREIGN KEY(actorMovieActorId) REFERENCES actor(actorId),
+	FOREIGN KEY (actorMovieMovieId) REFERENCES movie(movieId),
+	PRIMARY KEY (actorMovieActorId, actorMovieMovieId)
+);
 
 -- Create movie entity; movieId is the primary key
-
 CREATE TABLE movie (
 	-- movieId is the primary key
 	movieId INT UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -28,4 +40,18 @@ CREATE TABLE quote (
 	quoteContent VARCHAR (1000) NOT NULL,
 	PRIMARY KEY(quoteId)
 
+);
+
+-- Creating weak entity quoteActor here
+-- Many quotes can have many actors
+CREATE TABLE quoteActor (
+	quoteActorActorId INT UNSIGNED NOT NULL,
+	quoteActorQuoteId INT UNSIGNED NOT NULL,
+	-- Indexing foreign keys
+	INDEX(quoteActorActorId),
+	INDEX(quoteActorQuoteId),
+	-- Foreign Key relations
+	FOREIGN KEY(quoteActorActorId) REFERENCES actor(actorId),
+	FOREIGN KEY(quoteActorQuoteId) REFERENCES quote(quoteId),
+	PRIMARY KEY(quoteActorActorId, quoteActorQuoteId)
 );
