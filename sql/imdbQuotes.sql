3a3a909c3f2157defcc31d733529dbea0b21f231
Victoria Chacon <Vchaco06@gmail.com>
2016-07-26 09:42:47 -0600

    Recoded entities so that it reflects my first ERD

--- sql/imdbQuotes.sql
+++ sql/imdbQuotes.sql
@@ -3,42 +3,29 @@ DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS quote;
 
 -- Creating tables for my entities here
+-- Create actor entity; actorId is the primary key
 CREATE TABLE actor (
 	actorId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	actorName VARCHAR (100) NOT NULL,
-	-- Is the order of movieActorId relevant here?? why not in the movie table
-	movieActorId INT UNSIGNED NOT NULL,
-	INDEX(movieActorId),
-	FOREIGN KEY(movieActorId) REFERENCES actor(actorId),
-	UNIQUE(actorId),
 	PRIMARY KEY(actorId)
 
 );
--- Create movie entity
+-- Creating weak entity actorMovie here
+-- Many quotes can have many actors
+
+-- Create movie entity; movieId is the primary key
 
 CREATE TABLE movie (
 	-- movieId is the primary key
 	movieId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	movieTitle VARCHAR (128) NOT NULL,
-	-- movieActorId is meant to connect movie to actor
-	movieActorId INT UNSIGNED NOT NULL,
-	-- made so no duplicate movie Id's can exist
-	UNIQUE(movieId),
 	PRIMARY KEY(movieId)
 
 );
--- Creating the table for a weak entity here
+-- Create quote entity; quoteId is the primary key here
 CREATE TABLE quote (
-	quoteActorId INT UNSIGNED NOT NULL,
+	quoteId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	quoteContent VARCHAR (1000) NOT NULL,
-	quoteMovieId INT UNSIGNED NOT NULL,
-	-- Indexing foreign keys
-	INDEX(quoteActorId),
-	INDEX(quoteMovieId),
-	-- foreign key relations
-	FOREIGN KEY(quoteActorId) REFERENCES actor(actorId),
-	FOREIGN KEY(quoteMovieId) REFERENCES movie(movieId),
-	-- making composite foreign key using two foreign keys
-	PRIMARY KEY(quoteActorId, quoteMovieId)
+	PRIMARY KEY(quoteId)
 
 );
